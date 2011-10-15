282a43f2e7e80647fa033fb455f83a40302cb5ec
berger killer <bergerkiller@gmail.com>
2011-10-15 14:15:57 +0200

    Added cancelled check

--- src/com/bergerkiller/bukkit/sr/SRBlockListener.java
+++ src/com/bergerkiller/bukkit/sr/SRBlockListener.java
@@ -11,27 +11,29 @@ public class SRBlockListener extends BlockListener {
 	
 	@Override
 	public void onBlockFromTo(BlockFromToEvent event) {
-		Block b = event.getToBlock();
-		if (b.getTypeId() == 0) {
-			Block f = event.getBlock();
-			int fid = f.getTypeId();
-			if (fid == 9 || (allowLava && fid == 11)) {
-				//check surrounding blocks: 2 are water?
-				//ignore the from block (we know it is water)
-				for (BlockFace face : new BlockFace[] {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST}) {
-					Block r = b.getRelative(face);
-					if (f.getX() == r.getX()) {
-						if (f.getY() == r.getY()) {
-							if (f.getZ() == r.getZ()) {
-								continue;
+		if (!event.isCancelled()) {
+			Block b = event.getToBlock();
+			if (b.getTypeId() == 0) {
+				Block f = event.getBlock();
+				int fid = f.getTypeId();
+				if (fid == 9 || (allowLava && fid == 11)) {
+					//check surrounding blocks: 2 are water?
+					//ignore the from block (we know it is water)
+					for (BlockFace face : new BlockFace[] {BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST}) {
+						Block r = b.getRelative(face);
+						if (f.getX() == r.getX()) {
+							if (f.getY() == r.getY()) {
+								if (f.getZ() == r.getZ()) {
+									continue;
+								}
 							}
 						}
-					}
-					int rid = r.getTypeId();
-					if (rid == 8 || (allowLava && rid == 10)) {
-						event.getToBlock().setTypeId(fid, true);
-						event.setCancelled(true);
-						break;
+						int rid = r.getTypeId();
+						if (rid == 8 || (allowLava && rid == 10)) {
+							event.getToBlock().setTypeId(fid, true);
+							event.setCancelled(true);
+							break;
+						}
 					}
 				}
 			}
